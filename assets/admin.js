3a94d59ff446d6f82de5021c0bc353a4119057fa
s85013534-alt <s85013534@gmail.com>
2026-05-31 17:01:33 +0800

    Add files via upload

--- assets/admin.js
+++ assets/admin.js
@@ -378,8 +378,13 @@ function downloadFile(filename, content, type) {
   const link = document.createElement("a");
   link.href = url;
   link.download = filename;
+  link.style.display = "none";
+  document.body.appendChild(link);
   link.click();
-  URL.revokeObjectURL(url);
+  window.setTimeout(() => {
+    link.remove();
+    URL.revokeObjectURL(url);
+  }, 1000);
 }
 
 function csvCell(value) {
